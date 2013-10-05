652c4b099f2f9e7856185ca1fdba50771942a134
Robert Hailey <rhailey@allogy.com>
2013-10-05 14:27:32 -0500

    v: 0.2

--- src/main/java/com/allogy/isqrl/services/IsqrlModule.java
+++ src/main/java/com/allogy/isqrl/services/IsqrlModule.java
@@ -20,7 +20,7 @@ import java.io.IOException;
  */
 public class IsqrlModule
 {
-    public static final String VERSION="UNAVAILABLE";
+    public static final String VERSION="0.2";
 
     public static void bind(ServiceBinder binder)
     {
