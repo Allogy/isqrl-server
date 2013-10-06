c595d5509ba7017a02c88a30cc933338f2bb2500
Robert Hailey <rhailey@allogy.com>
2013-10-05 21:10:35 -0500

    b: request time not needed

--- src/main/java/com/allogy/isqrl/services/IsqrlModule.java
+++ src/main/java/com/allogy/isqrl/services/IsqrlModule.java
@@ -9,10 +9,9 @@ import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
 import org.apache.tapestry5.ioc.annotations.Local;
-import org.apache.tapestry5.services.*;
-import org.slf4j.Logger;
-
-import java.io.IOException;
+import org.apache.tapestry5.services.ComponentEventResultProcessor;
+import org.apache.tapestry5.services.RequestFilter;
+import org.apache.tapestry5.services.Response;
 
 /**
  * This module is automatically included as part of the Tapestry IoC Registry, it's a good place to
@@ -70,7 +69,7 @@ public class IsqrlModule
      * service interface and would be "RequestFilter".  Since Tapestry already defines
      * a service named "RequestFilter" we use an explicit service id that we can reference
      * inside the contribution method.
-     */
+     * /
     public RequestFilter buildTimingFilter(final Logger log)
     {
         return new RequestFilter()
@@ -96,6 +95,7 @@ public class IsqrlModule
             }
         };
     }
+     */
 
     /**
      * This is a contribution to the RequestHandler service configuration. This is how we extend
