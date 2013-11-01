da5e268607fcd064acdab1afaea96bed3878bff3
Robert Hailey <rhailey@allogy.com>
2013-11-01 10:11:45 -0500

    f: robots.txt

--- src/main/java/com/allogy/isqrl/pages/Index.java
+++ src/main/java/com/allogy/isqrl/pages/Index.java
@@ -1,14 +1,64 @@
 package com.allogy.isqrl.pages;
 
+import com.allogy.isqrl.helpers.OutputStreamResponse;
+import org.apache.tapestry5.services.Response;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 /**
  * Start page of application isqrl.
  */
 public class Index
 {
-    /*
-    Object onActivate()
+
+    Object onActivate(String filename)
     {
-        return new TextStreamResponse("text/plain", "For more information, please visit: https://grc.com/sqrl/sqrl.htm");
+        if (filename.equals("robots.txt"))
+        {
+            return robotExclusionAdvisory;
+        }
+
+        return null;
     }
-    */
+
+    private static final
+    OutputStreamResponse robotExclusionAdvisory=new OutputStreamResponse()
+    {
+        public
+        String getContentType()
+        {
+            return "text/plain";
+        }
+
+        public
+        void writeToStream(OutputStream outputStream) throws IOException
+        {
+            PrintStream out=new PrintStream(outputStream);
+            try
+            {
+                out.println("User-agent: *");
+                out.println("Allow: /index/");
+                out.println("Disallow: /");
+
+                // Generated & random data is simply a waste...
+                out.println("Disallow: /qr");
+
+                // Must have been an elaborate robot to pick this url up from the javascript...
+                out.println("Disallow: /poll");
+            }
+            finally
+            {
+                out.close();
+            }
+        }
+
+        public
+        void prepareResponse(Response response)
+        {
+            //no-op...
+        }
+    };
+
 }
